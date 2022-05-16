77d75ed320168cfffe5db1fbab7371884ce486bb
wasiqibnzahid <wasiqibnzahid@gmail.com>
2022-05-16 09:57:04 -0700

    Vue page changed

--- Vue Practice/vue-practice/src/views/AboutView.vue
+++ Vue Practice/vue-practice/src/views/AboutView.vue
@@ -1,6 +1,22 @@
 <template>
-  <div @click="messageToggler" class="about">
-    <h1>{{ aboutMessage }}</h1>
+  <div class="about">
+    <h1 @click="messageToggler">{{ aboutMessage }}</h1>
+    <div class="table">
+      <table>
+        <tr>
+          <th>Author Name</th>
+          <th>Book Name</th>
+        </tr>
+        <tr v-for="book in books" :key="book.id">
+          <td>
+            {{ book.bookName }}
+          </td>
+          <td>{{ book.authorName }}</td>
+        </tr>
+      </table>
+      <button @click="addRow">Add Item</button>
+      <button @click="removeRow">Remove Item</button>
+    </div>
   </div>
 </template>
 <script>
@@ -8,6 +24,12 @@ export default {
   data() {
     return {
       aboutMessage: "Welcome to the about page",
+      books: [
+        { bookName: "Percy Jackson", authorName: "Super Writer Man" },
+        { bookName: "Peaky Blinders", authorName: "Alfie Solomons" },
+        { bookName: "Spider Man Chronicles", authorName: "Stan Lee" },
+        { bookName: "Batman", authorName: "Bruce Wayne" },
+      ],
     };
   },
   methods: {
@@ -18,6 +40,22 @@ export default {
         this.aboutMessage = "Welcome to the about page";
       }
     },
+    addRow() {
+      this.books.push({
+        bookName: "New Book",
+        authorName: "New Author",
+      });
+    },
+    removeRow() {
+      this.books.splice(-1, 1);
+    },
   },
 };
 </script>
+
+<style scoped>
+table {
+  text-align: left;
+  border-spacing: 100px;
+}
+</style>
