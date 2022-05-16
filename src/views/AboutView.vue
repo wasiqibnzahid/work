027d36ace7aa6536ffa112b149038f6c99dc3e7d
wasiqibnzahid <wasiqibnzahid@gmail.com>
2022-05-16 07:17:28 -0700

    Hello World

---
+++ src/views/AboutView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="about">
+    <h1>{{ aboutMessage }}</h1>
+    <router-link to="/">Home</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      aboutMessage: "Hello World but on about page",
+    };
+  },
+};
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h2 @click="messageChange">{{ initialMessage }}</h2>
+    <router-link to="/aboutSIES">About</router-link>
+    <ul>
+      <li v-for="book in books" :key="book.id">
+        <h3>{{ book.title }} from {{ book.author }}</h3>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomeView",
+
+  data() {
+    return {
+      initialMessage: "Hello World",
+      books: [
+        { author: "redman", title: "bigbook" },
+        { author: "blueman", title: "smallbook" },
+        { author: "blackman", title: "mediumbook" },
+      ],
+    };
+  },
+  methods: {
+    messageChange() {
+      this.initialMessage = "Bye World";
+    },
+  },
+};
+</script>
